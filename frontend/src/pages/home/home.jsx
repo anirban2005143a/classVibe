229acdafe760099790f9b0d91e8fe2b654ae20fd
anirban2005143a <23je0104@iitism.ac.in>
2025-01-26 15:38:23 +0530

    fgvtrG

--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
-import StudyDesk from './StudyDesk'
+import React, { useEffect, useRef } from 'react'
 import studyDeskImg from "../../assets/studyDesk.png"
+import gsap from 'gsap'
+import Loader from '../../components/loader/Loader'
 
 const Home = () => {
-  console.log(window.innerWidth)
+
+
+  useEffect(() => {
+    gsap.from("#home .introHeading", {
+      x: -100,
+      opacity: 0,
+      duration: 0.5
+    })
+    gsap.from("#home .introPara", {
+      x: 100,
+      opacity: 0,
+      duration: 0.5,
+      delay: 0.2
+    })
+    gsap.from("#home .startButton" , {
+      y:100,
+      opacity:0,
+      duration:0.5,
+      delay:0.4
+    })
+    gsap.from("#home .studyDeskImg" ,{
+      scale:0,
+      duration:0.5
+    })
+
+  }, [])
+
+
   return (
     <>
       <div id="home">
@@ -12,36 +40,38 @@ const Home = () => {
             <img src={studyDeskImg} alt="studyDesk" className='md:w-6/12 sm:w-8/12 w-11/12 object-cover ' />
           </div>
 
-          <div className=' absolute w-full h-full top-0 left-0 flex justify-start items-center'>
+          <div className=' intro absolute w-full h-full top-0 left-0 flex justify-start items-center'>
             <div className=' p-4 md:w-7/12 sm:w-10/12 w-full '>
-              <h2 className=' nunito text-5xl font-bold p-4'>CLASS VIBE</h2>
-              <p className='nunito text-xl p-4'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae numquam pariatur, voluptates sequi nostrum </p>
+              <h2 className=' introHeading nunito text-5xl font-bold p-4'>CLASS VIBE</h2>
+              <p className='introPara nunito text-xl p-4'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae numquam pariatur, voluptates sequi nostrum </p>
 
-              <button
-                className="relative m-4 flex items-center px-6 py-3 overflow-hidden font-medium transition-all bg-indigo-500 rounded-md group"
-              >
-                <span
-                  className="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-indigo-700 rounded group-hover:-mr-4 group-hover:-mt-4"
+              <div className='startButton'>
+                <button
+                  className="relative  m-4 flex items-center px-6 py-3 overflow-hidden font-medium transition-all bg-indigo-500 rounded-md group"
                 >
                   <span
-                    className="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-[#fff1e4]"
-                  ></span>
-                </span>
-                <span
-                  className="absolute bottom-0 rotate-180 left-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-indigo-700 rounded group-hover:-ml-4 group-hover:-mb-4"
-                >
+                    className="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-indigo-700 rounded group-hover:-mr-4 group-hover:-mt-4"
+                  >
+                    <span
+                      className="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-[#fff1e4]"
+                    ></span>
+                  </span>
                   <span
-                    className="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-[#fff1e4]"
+                    className="absolute bottom-0 rotate-180 left-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-indigo-700 rounded group-hover:-ml-4 group-hover:-mb-4"
+                  >
+                    <span
+                      className="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-[#fff1e4]"
+                    ></span>
+                  </span>
+                  <span
+                    className="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full bg-indigo-600 rounded-md group-hover:translate-x-0"
                   ></span>
-                </span>
-                <span
-                  className="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full bg-indigo-600 rounded-md group-hover:translate-x-0"
-                ></span>
-                <span
-                  className="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-white"
-                >Get Started</span
-                >
-              </button>
+                  <span
+                    className="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-white"
+                  >Get Started</span
+                  >
+                </button>
+              </div>
 
             </div>
           </div>
@@ -49,6 +79,10 @@ const Home = () => {
         {/* <div className="model studyDesk overflow-x-hidden">
           <StudyDesk/>
         </div> */}
+
+        <div className="features">
+          
+        </div>
       </div>
 
     </>
